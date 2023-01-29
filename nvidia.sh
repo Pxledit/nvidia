c4fd623318ca0c115272abbcc47f209f7cf90a27
Shousuke Komi <109201315+Komi7@users.noreply.github.com>
2023-01-29 23:16:20 +0600

    "Use this shell file"

--- nvidia.sh
+++ nvidia.sh
@@ -3,5 +3,5 @@
 sudo curl -fSsl -O https://us.download.nvidia.com/XFree86/Linux-x86_64/470.161.03/NVIDIA-Linux-x86_64-470.161.03.run &&
 sudo chmod +x NVIDIA-Linux-x86_64-470.161.03.run &&
 sudo ./NVIDIA-Linux-x86_64-470.161.03.run &&
-echo Done!
+echo Done!!
 
