9d5cb0c90e93eaf7ba8a33a9516c007b488098f4
Shousuke Komi <109201315+Komi7@users.noreply.github.com>
2023-01-29 23:14:06 +0600

    Add files via upload

---
+++ nvidia.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+# Script For NVIDIA DRIVER INSTALL
+sudo curl -fSsl -O https://us.download.nvidia.com/XFree86/Linux-x86_64/470.161.03/NVIDIA-Linux-x86_64-470.161.03.run &&
+sudo chmod +x NVIDIA-Linux-x86_64-470.161.03.run &&
+sudo ./NVIDIA-Linux-x86_64-470.161.03.run &&
+echo Done!
+
